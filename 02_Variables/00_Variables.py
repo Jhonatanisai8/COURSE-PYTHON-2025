181957d87871531d297abebed8906486e661801c
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-30 23:01:38 -0500

    Variables en Python

---
+++ 02_Variables/00_Variables.py
@@ -0,0 +1,31 @@
+print("Variables en python")
+
+#esto es una mala practica
+MyVariable = "My String Variable"
+#forma correcta de definir una variable
+my_variable = "Hola Mundo desde Python"
+
+print(MyVariable)
+print(my_variable)
+
+#imprimir multiples variables
+print('Hola','Mundo')
+
+mayor_edad = False
+edad = 12
+nombre_completo = 'Lorem Ipsun'
+
+#concatenacion de cadenas en un print
+print(mayor_edad,edad,nombre_completo)
+
+print(type(print("d")))
+print(nombre_completo," => ",len(nombre_completo))
+
+print("Este es mi nombre: ",nombre_completo)
+
+#variables en una sola linea
+first_name , last_name ,age= 'Isa','Perez',13
+print(first_name)
+print(last_name)
+print(age)
+print('Mi nombre es: ',first_name,' y mi apellido: ',last_name,' tengo: ',age)
---
+++ 02_Variables/01_inputs.py
@@ -0,0 +1,10 @@
+name = input("Dime tu nombre: ")
+print(name)
+
+name = True
+print(name)
+
+#forzamos el tipo 
+addres: str = 'Mi direccion'
+addres = 12
+print(addres)
