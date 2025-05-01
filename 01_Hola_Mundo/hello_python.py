b3e7f4ed8adf6910741e0db8b8afe12a43811ce4
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-30 23:01:30 -0500

    hola Mundo

---
+++ 01_Hola_Mundo/hello_python.py
@@ -0,0 +1,23 @@
+# esto es un comentario
+print("Hola Mundo")
+mi_nombre = 'isai'
+print(mi_nombre)
+
+"""
+Este es un comentario multilinea
+"""
+
+'''
+Esto es un comentario multilinea
+'''
+
+print("Hola Python")
+# consultar el tipo de dato en python
+print(type("Hola")) #str
+print(type(1.1))    #float
+print(type(1))      #int
+print(type([9,2]))  #list
+print(type((2,3)))  #tuple
+print(type({2,3}))  #set
+print(type(True))   #bool
+print(type(2+2j))   #complex
